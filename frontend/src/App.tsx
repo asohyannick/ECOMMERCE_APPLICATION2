8ae4d203db0e0a73320976c3ad0f4df7afd2d599
asohyannick <asohyannick3@gmail.com>
2024-09-23 16:34:01 -0700

    local development environment has been setup both on the  frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Start building the UI  right away  developer
+    </h1>
+    </>
+  )
+}
+
+export default App
